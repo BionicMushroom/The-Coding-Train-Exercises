7b12f7fa1ff75bf4847a5339798ae599eba4d4fb
Dumitrescu Tiberiu Alexandru <tiberiudumitrescu@yahoo.com>
2020-08-06 22:06:03 +0300

    [Zoog, MouseTracker & CircleMorphing]: Added noexcept to wWinMain and view_factory's defaulted methods.

--- CircleMorphing/Sources/view_factory.h
+++ CircleMorphing/Sources/view_factory.h
@@ -5,11 +5,13 @@ struct view_factory
 {
 	view_factory(const winrt::Windows::ApplicationModel::Core::IFrameworkView& framework_view) noexcept;
 
-	view_factory(const view_factory& other) = default;
-	view_factory(view_factory&& other) = default;
+	view_factory(const view_factory& other) noexcept = default;
+	view_factory(view_factory&& other) noexcept = default;
 
-	view_factory& operator= (const view_factory& other) = default;
-	view_factory& operator= (view_factory&& other) = default;
+	~view_factory() noexcept = default;
+
+	view_factory& operator= (const view_factory& other) noexcept = default;
+	view_factory& operator= (view_factory&& other) noexcept = default;
 
 	winrt::Windows::ApplicationModel::Core::IFrameworkView CreateView() const noexcept;
 
--- MouseTracker/Sources/main.cpp
+++ MouseTracker/Sources/main.cpp
@@ -6,7 +6,7 @@
 using namespace winrt::Windows::ApplicationModel::Core;
 
 int WINAPI wWinMain([[maybe_unused]] HINSTANCE instance, [[maybe_unused]] HINSTANCE previous_instance,
-	[[maybe_unused]] PWSTR command_line, [[maybe_unused]] int show_command)
+	[[maybe_unused]] PWSTR command_line, [[maybe_unused]] int show_command) noexcept
 {
 	CoreApplication::Run(winrt::make<view_factory>(winrt::make<main_view>()));
 	return 0;
